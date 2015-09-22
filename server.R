313c29fa1aeff4a47157ab8515b48b50089ee4b5
yachna kapoor <yac.chadha.gmail.com>
2015-09-22 22:56:28 +0530

    commit
    commit

---
+++ server.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+fx <- function(x,y) {
+  data <- mtcars
+  tmp <- data[data$cyl == x   , ]
+  tmp <- data[data$cyl == x &
+                data$hp <= y, ]
+  out <- mean(tmp$mpg)
+  return (out)
+}
+
+shinyServer(
+  function(input, output) {
+    output$mpgId <- renderPrint(fx(input$cyl, input$horse))
+  }
+)
